eb7dc457554bf8cd1256fb50dfb2c26457f7c460
John Moustakas <john.moustakas@gmail.com>
2019-04-24 06:26:54 -0400

    dynesty fitting working

--- py/hizea/prospector.py
+++ py/hizea/prospector.py
@@ -8,99 +8,28 @@ Code to do SED-fitting with Prospector.
 import os, time
 import numpy as np
 
-def load_obs(snr=10):
-    """
-    Load the photometry for J2118.
-    
-    From Christy:
-      W1      14.8739    0.0154054
-      W2      14.1342    0.0283280
-      W3      10.4872    0.0699660
-      W4      8.02651     0.179578
-    
-    """
-    import sedpy
-    from prospect.utils.obsutils import fix_obs    
-
-    phot = dict(
-        FUV=(1.82491e-09, 1.15115e+19),
-        NUV=(8.06441e-09, 2.25963e+19),
-        u=(1.27666e-08, 1.53319e+18),
-        g=(1.59991e-08, 7.47418e+18),
-        r=(3.15573e-08, 2.18797e+18),
-        i=(3.63049e-08, 1.53877e+18),
-        z=(4.14564e-08, 2.71207e+17),
-        ch1=(9.25971e-08, 3.91873e+17),
-        ch2=(9.67009e-08, 3.54276e+17),
-        W1=(9.40651e-08, 5.61366e+17),
-        W2=(1.02882e-07, 1.38784e+17),
-        W3=(5.44324e-07, 8.12757e+14),
-        W4=(1.38524e-06, 1.90498e+13))    
-
-    galex = ['galex_FUV', 'galex_NUV']
-    sdss = ['sdss_{}0'.format(b) for b in ['u','g','r','i','z']]
-    spitzer = ['spitzer_irac_ch{}'.format(n) for n in ['1','2']]
-    wise = ['wise_w{}'.format(n) for n in ['1','2', '3', '4']]
-    filternames = galex + sdss + spitzer + wise
-
-    obs = {}
-    obs['redshift'] = 0.535
-    obs["filters"] = sedpy.observate.load_filters(filternames)
-
-    obs["maggies"] = np.array([phot[filt][0] for filt in phot.keys()])
-    obs["maggies_unc"] = np.array([1/np.sqrt(phot[filt][1]) for filt in phot.keys()])
-
-    # mask out W4
-    #obs["phot_mask"] = np.array(['w4' in f.name for f in obs["filters"]])    
-    
-    # Create a handy vector of effective wavelengths (optional) 
-    obs["phot_wave"] = [f.wave_effective for f in obs["filters"]]
-    obs["wavelength"] = None # spectral wavelength
-    obs["spectrum"] = None
-    obs['unc'] = None  # spectral uncertainties are given here
-    obs['mask'] = None
-    obs = fix_obs(obs)
-
-    run_params = {}
-    run_params['redshift'] = obs['redshift']
-    run_params["verbose"] = True
-
-    run_params["nmin"] = 5
-    run_params['ftol'] = 3e-16 
-    run_params['maxfev'] = 500
-    run_params['xtol'] = 3e-16
-    run_params["min_method"] = 'levenberg_marquardt'
-
-    # dynesty Fitter parameters
-    dyn_params = {'nested_bound': 'multi', # bounding method
-              'nested_sample': 'unif', # sampling method
-              'nested_nlive_init': 100,
-              'nested_nlive_batch': 100,
-              'nested_bootstrap': 0,
-              'nested_dlogz_init': 0.05,
-              'nested_weight_kwargs': {"pfrac": 1.0},
-              'nested_stop_kwargs': {"post_thresh": 0.1}
-              }
-    run_params.update(dyn_params)
-    
-    return obs, run_params
-
-def load_sps(zcontinuous=1):
+def load_sps(zcontinuous=1, verbose=False):
     """zcontinuous - interpolate between metallicity values.
 
     """
     from prospect.sources import CSPSpecBasis
+    t0 = time.time()
     sps = CSPSpecBasis(zcontinuous=zcontinuous)
+    if verbose:
+        print('Loading SPS models took {:.2f} sec'.format(time.time()-t0))
     return sps
 
-def load_model(template_library='delayed-tau', redshift=0.0):
+def load_model(template_library='delayed-tau', redshift=0.0, verbose=False):
     
     from prospect.models import priors
     from prospect.models.sedmodel import SedModel
     from prospect.models.templates import TemplateLibrary
 
     if template_library == 'delayed-tau':
-        model_params = TemplateLibrary["parametric_sfh"]
+        if verbose:
+            TemplateLibrary.describe('parametric_sfh')
+
+        model_params = TemplateLibrary['parametric_sfh']        
 
         # Initial values
         model_params["zred"]['init'] = redshift
@@ -114,10 +43,10 @@ def load_model(template_library='delayed-tau', redshift=0.0):
 
         # Prior ranges
         model_params['mass']['prior'] = priors.LogUniform(mini=1e10, maxi=1e12)
-        model_params['tau']['prior'] = priors.LogUniform(mini=1, maxi=1e2)
-        model_params['tage']['prior'] = priors.LogUniform(mini=0.001, maxi=10)
+        model_params['tau']['prior'] = priors.LogUniform(mini=1, maxi=10)
+        model_params['tage']['prior'] = priors.LogUniform(mini=0.1, maxi=10)
         model_params['dust2']['prior'] = priors.TopHat(mini=0.0, maxi=2.0)
-        model_params['logzsol']['prior'] = priors.TopHat(mini=-1.0, maxi=0.3)
+        model_params['logzsol']['prior'] = priors.TopHat(mini=-0.5, maxi=0.2)
 
         # If we are going to be using emcee, it is useful to provide a 
         # minimum scale for the cloud of walkers (the default is 0.1)
@@ -195,59 +124,72 @@ def chivecfn(theta, model, obs, sps, verbose):
     
     return np.concatenate([chispec, chiphot])
 
-def max_likelihood(run_params, model, obs, sps, seed=1, verbose=True):
+def run_minimize(model, obs, sps, seed=1, nmin=10, xtol=5e-16, ftol=5e-16,
+                 maxnfev=500, min_method='trf', nproc=1, verbose=True, pool=None):
     """Simple maximum likelihood fitting.
 
     """
-    from prospect import fitting
+    import multiprocessing
     from scipy.optimize import least_squares
+    from prospect import fitting
 
-    # We'll start minimization from "nmin" separate places, 
-    # the first based on the "init" values of each parameter and the 
-    # rest drawn from the prior.  This can guard against local minima.
-    ts = time.time()  # time it
-    pinitial = fitting.minimizer_ball(model.initial_theta.copy(),
-                                      run_params['nmin'], model,
-                                      seed=seed)
-    guesses = []
-    for i, pinit in enumerate(pinitial): #loop over initial guesses
-        res = least_squares(chivecfn, np.array(pinit), 
-                            method='dogbox', x_scale='jac',
-                            xtol=run_params["xtol"], ftol=run_params["ftol"], 
-                            max_nfev=run_params["maxfev"],
-                            args=(model, obs, sps, verbose))
-        guesses.append(res)
-
-    # Calculate chi-square of the results, and choose the best one
-    # fitting.reinitialize moves the parameter vector away from edges of the prior.
-    chisq = [np.sum(r.fun**2) for r in guesses]
-    best = np.argmin(chisq)
-    theta_best = fitting.reinitialize(guesses[best].x, model,
-                                      edge_trunc=run_params.get('edge_trunc', 0.1))
-    initial_prob = None
-    pdur = time.time() - ts
-
-    # output results
     if verbose:
-        print('done {0} in {1}s'.format(run_params['min_method'], pdur))
-        print('best {0} chi-sq: {1}'.format(run_params['min_method'], chisq[best]))
-        print('best guess paramaters:')
+        print('Minimization method: {}, number of initial tries: {}, number of cores: {}.'.format(
+            min_method, nmin, nproc))
+
+    # Start minimization from "nmin" separate places, the first based on
+    # the "init" values of each parameter and the rest drawn from the prior.
+    qinit = fitting.minimizer_ball(model.theta.copy(), nmin,
+                                   model, seed=seed)
+
+    pool = multiprocessing.Pool(nproc)
+
+    min_opts = {'xtol': xtol, 'ftol': ftol, 'max_nfev': maxnfev}
+
+    t0 = time.time()
+    with np.errstate(invalid='ignore'):
+        res = list(pool.map([least_squares(chivecfn, np.array(qq), method=min_method,
+                                           x_scale='jac', **min_opts,
+                                           args=(model, obs, sps, verbose)) for qq in qinit]))
+    dt = time.time() - t0
+
+    pool.close()
+    
+    chisq = [np.sum(rr.fun**2) for rr in res]
+    chi2min = np.argmin(chisq)
+    theta_best = fitting.reinitialize(res[best].x, model)
+
+    if verbose:
+        print('{} minimization took {:.3f} seconds.'.format(min_method, dt))
+        print('Chi2min = {:.3f} '.format(chi2min))
+        print('Best-fitting paramaters:')
         for k, t in zip(model.theta_labels(), theta_best):
             print('  {} = {}'.format(k, t))
 
-    return theta_best
+    return theta_best, chi2min
 
-def dynesty_solve(run_params, model, nested=False):
+def run_dynesty(run_params, model, nested=False):
 
     from prospect import fitting
     from dynesty.dynamicsampler import stopping_function, weight_function
 
+    def prior_transform(u):        
+        return model.prior_transform(u)
+
     t0 = time.time()  # time it
-    out = fitting.run_dynesty(obs, model, sps, lnprobfn=lnprobfn, prior_transform, 
+
+
+    out = fitting.run_dynesty(obs, model, sps, lnprobfn=lnprobfn, 
                               stop_function=stopping_function,
                               wt_function=weight_function,
                               logl_args=(model, obs, sps, nested, verbose),
                               **run_params)
+
+    out = fitting.run_dynesty_sampler(lnprobfn, prior_transform, model.ndim,
+                                  stop_function=stopping_function,
+                                  wt_function=weight_function,
+                                  **run_params)
+    
 
     #def prior_transform(u, model=model):
     #    return model.prior_transform(u)
