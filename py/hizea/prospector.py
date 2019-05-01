0adc6232d417c28a299b70d1f4b81f1050dd1a5a
John Moustakas <john.moustakas@gmail.com>
2019-05-01 11:37:39 -0400

    debugging; more on priors

--- py/hizea/prospector.py
+++ py/hizea/prospector.py
@@ -8,6 +8,110 @@ Code to do SED-fitting with Prospector.
 import os, time
 import numpy as np
 
+def logmass2mass(logmass=11.0, **extras):
+    return 10**logmass
+
+def load_model(template_library='delayed-tau', redshift=0.0, verbose=False):
+    """
+    http://dfm.io/python-fsps/current/stellarpop_api/#api-reference
+    https://github.com/moustakas/siena-astrophysics/blob/master/research/redmapper/redmapper-stellar-mass.py#L125-L197    
+    
+    """
+    from prospect.models import priors
+    from prospect.models.sedmodel import SedModel
+    from prospect.models.templates import TemplateLibrary
+    from prospect.models.transforms import dustratio_to_dust1
+
+    def base_delayed_tau():
+        model_params = TemplateLibrary['parametric_sfh']
+
+        # Initialize with sensible numbers.
+        model_params['tau']['init'] = 10.0
+        model_params['tage']['init'] = 1.0
+        model_params['logzsol']['init'] = 0.2
+
+        model_params['logmass'] = {'N': 1, 'isfree': True, 'init': 11.0,
+                                   'prior': priors.TopHat(mini=10.0, maxi=12.0),
+                                   'units': '$M_{\odot}$'}
+
+        model_params['mass']['isfree'] = False
+        model_params['mass']['init'] = 10**model_params['logmass']['init']
+        model_params['mass']['prior'] = None
+        model_params['mass']['depends_on'] = logmass2mass
+        
+        # Adjust the prior ranges.
+        model_params['tau']['prior'] = priors.LogUniform(mini=0.1, maxi=30.0)
+        model_params['tage']['prior'] = priors.LogUniform(mini=0.01, maxi=10.0)
+        model_params['logzsol']['prior'] = priors.TopHat(mini=-0.5, maxi=0.2)
+
+        #print('HACK!!!!!!!!!!!!!')
+        model_params['tau']['isfree'] = False
+        model_params['tage']['isfree'] = False
+        model_params['logzsol']['isfree'] = False
+        model_params['dust2']['isfree'] = False
+
+        return model_params
+
+    if template_library == 'delayed-tau':
+        # Underlying delayed tau model.
+        model_params = base_delayed_tau()
+
+    if template_library == 'bursty':
+        # Underlying delayed tau model.
+        model_params = base_delayed_tau()
+        
+        # Add bursts
+        model_params.update(TemplateLibrary['burst_sfh'])
+        
+        #model_params['tburst']['isfree'] = True
+        #model_params['tburst']['init'] = 8.0
+        #model_params['tburst']['prior'] = priors.TopHat(mini=0.0, maxi=10.0)
+
+        model_params['fburst']['isfree'] = True
+        model_params['fburst']['init'] = 0.1
+        model_params['fburst']['prior'] = priors.TopHat(mini=0.0, maxi=1.0)
+
+        model_params['fage_burst']['isfree'] = True
+        model_params['fage_burst']['init'] = 0.9
+        model_params['fage_burst']['prior'] = priors.TopHat(mini=0.5, maxi=1.0)
+
+    # Add dust emission (with fixed dust SED parameters).
+    #model_params.update(TemplateLibrary['dust_emission'])
+
+    model_params['dust2']['init'] = 1.0 # diffuse dust
+    model_params['dust2']['prior'] = priors.TopHat(mini=0.0, maxi=4.0)
+
+    ## Add more dust flexibility.
+    #model_params['dust_type'] = {'N': 1, 'isfree': False, 'init': 0, 'units': 'dust model'}
+    #model_params['dust_index'] = {'N': 1, 'isfree': False, 'init': -0.7,
+    #                              'units': 'power-law index', 'prior': None}
+    #
+    #model_params['dust1'] = {'N': 1, 'isfree': False, 'init': 0.0, 'prior': None,
+    #                         'units': 'optical depth towards young stars',
+    #                         'depends_on': dustratio_to_dust1}
+    #model_params['dust_ratio'] = {'N': 1, 'isfree': True, 'init': 1.0,
+    #                              'prior': priors.TopHat(mini=1.0, maxi=10.0),
+    #                              'units': 'dust1/dust2 ratio (optical depth to young stars vs diffuse)'}
+
+    ## Add nebular emission.
+    #model_params.update(TemplateLibrary['nebular'])
+    ##model_params['add_neb_continuum']['init'] = False
+    #model_params['gas_logu']['init'] = -1.0 # harder radiation field [default is -2.0]
+
+    # Fixed redshift.
+    model_params['zred']['init'] = redshift
+    model_params['zred']['isfree'] = False 
+
+    # Change the IMF from Kroupa to Salpeter.
+    #model_params['imf_type']['init'] = 0
+        
+    # Now instantiate the model using this new dictionary of parameter specifications
+    model = SedModel(model_params)
+    if verbose:
+        print(model)
+
+    return model
+
 def load_sps(zcontinuous=1, verbose=False):
     """zcontinuous - interpolate between metallicity values.
 
@@ -18,51 +122,6 @@ def load_sps(zcontinuous=1, verbose=False):
     if verbose:
         print('Loading SPS models took {:.2f} sec'.format(time.time()-t0))
     return sps
-
-def load_model(template_library='delayed-tau', redshift=0.0, verbose=False):
-    
-    from prospect.models import priors
-    from prospect.models.sedmodel import SedModel
-    from prospect.models.templates import TemplateLibrary
-
-    if template_library == 'delayed-tau':
-        if verbose:
-            TemplateLibrary.describe('parametric_sfh')
-
-        model_params = TemplateLibrary['parametric_sfh']        
-
-        # Initial values
-        model_params["zred"]['init'] = redshift
-        model_params["mass"]["init"] = 3e11
-        model_params["tau"]["init"] = 10.0
-        model_params["tage"]["init"] = 1.0
-        model_params["dust2"]["init"] = 1.0
-        model_params["logzsol"]['init'] = 0.1
-        
-        model_params["zred"]['isfree'] = False # fixed redshift
-
-        # Prior ranges
-        model_params['mass']['prior'] = priors.LogUniform(mini=1e10, maxi=1e12)
-        model_params['tau']['prior'] = priors.LogUniform(mini=1, maxi=10)
-        model_params['tage']['prior'] = priors.LogUniform(mini=0.1, maxi=10)
-        model_params['dust2']['prior'] = priors.TopHat(mini=0.0, maxi=2.0)
-        model_params['logzsol']['prior'] = priors.TopHat(mini=-0.5, maxi=0.2)
-
-        # If we are going to be using emcee, it is useful to provide a 
-        # minimum scale for the cloud of walkers (the default is 0.1)
-        model_params["mass"]["disp_floor"] = 1e9
-        model_params["tau"]["disp_floor"] = 1.0
-        model_params["tage"]["disp_floor"] = 0.05
-        model_params['dust2']['disp_floor'] = 0.1
-        model_params['logzsol']['disp_floor'] = 0.05
-
-        # Add dust emission (with fixed dust SED parameters)
-        model_params.update(TemplateLibrary["dust_emission"])
-        
-    # Now instantiate the model using this new dictionary of parameter specifications
-    model = SedModel(model_params)
-
-    return model
 
 def lnprobfn(theta, model, obs, sps, nested=False, verbose=False):
     """
@@ -185,20 +244,6 @@ def run_dynesty(run_params, model, nested=False):
                               logl_args=(model, obs, sps, nested, verbose),
                               **run_params)
 
-    out = fitting.run_dynesty_sampler(lnprobfn, prior_transform, model.ndim,
-                                  stop_function=stopping_function,
-                                  wt_function=weight_function,
-                                  **run_params)
-    
-
-    #def prior_transform(u, model=model):
-    #    return model.prior_transform(u)
-    #
-    #out = fitting.run_dynesty_sampler(lnprobfn, prior_transform, model.ndim,
-    #                                  stop_function=stopping_function,
-    #                                  wt_function=weight_function,
-    #                                  logl_args=(model, obs, sps, nested, verbose),
-    #                                  **run_params)
     print('done sampling in {:.3f} min'.format((time.time() - t0)/60))
 
     return out
